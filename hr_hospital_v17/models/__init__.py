6ee9444dfcf5c9b75af6952bcbef3807ff5aed98
kozlovvladimir <kozlovladimirgreen@gmail.com>
2025-02-19 18:47:25 +0200

    Removed hr_hospital_v17 from repository

--- hr_hospital_v17/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import hr_hospital_person
-from . import hr_hospital_doctor
-from . import hr_hospital_patient
-from . import hr_hospital_disease_type
-from . import hr_hospital_appointment
-from . import hr_hospital_diagnosis
--- hr_hospital_v17/wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import hr_hospital_change_doctor_wizard
-from . import hr_hospital_diagnosis_report_wizard
